cf835e13a7b24f1b83113a0b0835b92fca93b928
Jun <Jun@DESKTOP-5ELTOKB>
2024-08-07 09:33:51 -0700

    Add project files.

---
+++ MortgageCalculation.cs
@@ -0,0 +1,20 @@
+﻿namespace MortgageCalculator
+{
+    public class MortgageCalculation
+    {
+        //fields
+        private int Term; //months
+        private double InterestRate; //interest rate in % or not
+        private double Loan; //same as total principal for our purposes
+        
+        //properties
+        public double RemainingBalance { get; set; } // Previous remaining balance - principal payment
+        public double InterestPayment { get; set; } // previous remaining balance * interstRate/1200
+        public double PrincipalPayment { get; set; } // total monthly payment - interest payment | loan amount
+
+        //constructor
+        
+
+        //methods
+    }
+}
