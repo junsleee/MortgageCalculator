69c67e4b52c39e9f14c1fe4f6e1d01cabfff3442
junsleee <junslee1204@gmail.com>
2024-08-08 10:11:30 -0700

    cleaned up file directory

---
+++ MortgageCalculatorTests1/MortgageCalculationTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MortgageCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator.Tests
+{
+    [TestClass()]
+    public class MortgageCalculationTests
+    {
+        [TestMethod()]
+        public void CalculateMonthlyPaymentsTest()
+        {
+            //arrange
+            int expectedTerm = 360;
+            double expectedInterestRate = 4.5;
+            double expectedLoan = 200000;
+
+            //act
+            MortgageCalculation mortgage = new MortgageCalculation(expectedTerm, expectedInterestRate, expectedLoan);
+
+            //assert
+            Assert.AreEqual(expectedTerm, mortgage.Term);
+            Assert.AreEqual(expectedInterestRate, mortgage.InterestRate);
+            Assert.AreEqual(expectedLoan, mortgage.Loan);
+        }
+    }
+}
