fcd93d69ddcc1ab68f5c541525d25649e2beb2bc
junsleee <junslee1204@gmail.com>
2024-08-08 10:13:17 -0700

    created console

---
+++ MortgageCalculatorConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MortgageCalculatorConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
